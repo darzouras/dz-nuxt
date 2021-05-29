f60efcf02ef689d7d18595a3369a9339dacc9aec
Darlene Zouras <dzouras@gmail.com>
2021-05-29 09:40:51 -0500

    Starting to implement styles
    And figuring out how to pull in data

---
+++ components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="border-2 border-purple-100 p-1 md:mr-4">
+        {{ pages }}
+        <ul>
+            <li v-for="page in pages" :key="page.slug">
+                <NuxtLink :to="page.slug">{{ page.title }}</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        pages: {
+            Array
+        }
+    }
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="font-sans w-full min-h-screen bg-white color-purple-50">
-        <main>
-            <transition name="fade">
-                <Nuxt />
-            </transition>
-        </main>
+        <div class="w-11/12 md:w-10/12 py-5 md:py-10 mx-auto md:flex md:flex-wrap">
+            <header class="md:w-1/5">
+                <Nav />
+                <!-- TODO: page data is called only on page-level components, maybe use named slot to get the nav content in by page -->
+            </header>
+            <main class="md:w-4/5 lg:w-3/5">
+                <transition name="fade">
+                    <Nuxt />
+                </transition>
+            </main>
+            <footer class="w-full lg:w-1/5">
+                I'm a footer?
+            </footer>
+        </div>
     </div>
 </template>
+
+<script>
+import Nav from '../components/Nav.vue'
+
+export default {
+    components: {
+        Nav,
+    },
+}
+</script>
 
 <style>
 .fade-enter-active {
