ba9af0be798576dfb418b71866ffb9d92b43a6c0
Darlene Zouras <dzouras@gmail.com>
2021-05-30 21:06:26 -0500

    some small style adjustments

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="border-2 border-purple-100 p-1 md:mr-4">
+    <nav class="border-2 border-purple-100 p-1 md:mr-4 md:sticky md:top-10">
         <ul>
             <li v-for="page in navlist" :key="page.slug">
                 <NuxtLink :to="page.slug">{{ page.title }}</NuxtLink>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="font-sans w-full min-h-screen bg-white color-purple-50">
-        <div class="w-11/12 md:w-10/12 py-5 md:py-10 mx-auto md:flex md:flex-wrap">
+        <div class="w-11/12 md:w-10/12 py-5 md:py-10 mx-auto md:flex md:flex-wrap md:justify-between md:align-top lg:justify-center">
             <header class="md:w-1/5">
                 <Nav />
                 <!-- TODO: page data is called only on page-level components, maybe use named slot to get the nav content in by page -->
             </header>
             <main class="md:w-4/5 lg:w-3/5">
                 <transition name="fade">
-                    <Nuxt />
+                    <Nuxt class="max-w-screen-sm m-auto" />
                 </transition>
             </main>
             <footer class="w-full lg:w-1/5">
